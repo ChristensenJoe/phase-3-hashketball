e30ddef67f1c98c114b158e796df3727bfad8f43
Joe Christensen <joechristensenwork@gmail.com>
2021-08-09 15:28:35 -0700

    commit tree fb24f03bdbba46a14517d03a94b1a277ab7ec69a

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require "pry"
 # Write your code below game_hash
 def game_hash
   {
@@ -14,7 +15,7 @@ def game_hash
           assists: 12,
           steals: 3,
           blocks: 1,
-          slam_dunks: 1
+          slam_dunks: 1,
         },
         {
           player_name: "Reggie Evans",
@@ -25,7 +26,7 @@ def game_hash
           assists: 12,
           steals: 12,
           blocks: 12,
-          slam_dunks: 7
+          slam_dunks: 7,
         },
         {
           player_name: "Brook Lopez",
@@ -36,7 +37,7 @@ def game_hash
           assists: 10,
           steals: 3,
           blocks: 1,
-          slam_dunks: 15
+          slam_dunks: 15,
         },
         {
           player_name: "Mason Plumlee",
@@ -47,7 +48,7 @@ def game_hash
           assists: 6,
           steals: 3,
           blocks: 8,
-          slam_dunks: 5
+          slam_dunks: 5,
         },
         {
           player_name: "Jason Terry",
@@ -58,9 +59,9 @@ def game_hash
           assists: 2,
           steals: 4,
           blocks: 11,
-          slam_dunks: 1
-        }
-      ]
+          slam_dunks: 1,
+        },
+      ],
     },
     away: {
       team_name: "Charlotte Hornets",
@@ -75,7 +76,7 @@ def game_hash
           assists: 1,
           steals: 2,
           blocks: 7,
-          slam_dunks: 2
+          slam_dunks: 2,
         },
         {
           player_name: "Bismack Biyombo",
@@ -86,7 +87,7 @@ def game_hash
           assists: 7,
           steals: 22,
           blocks: 15,
-          slam_dunks: 10
+          slam_dunks: 10,
         },
         {
           player_name: "DeSagna Diop",
@@ -97,7 +98,7 @@ def game_hash
           assists: 12,
           steals: 4,
           blocks: 5,
-          slam_dunks: 5
+          slam_dunks: 5,
         },
         {
           player_name: "Ben Gordon",
@@ -108,7 +109,7 @@ def game_hash
           assists: 2,
           steals: 1,
           blocks: 1,
-          slam_dunks: 0
+          slam_dunks: 0,
         },
         {
           player_name: "Kemba Walker",
@@ -119,11 +120,84 @@ def game_hash
           assists: 12,
           steals: 7,
           blocks: 5,
-          slam_dunks: 12
-        }
-      ]
-    }
+          slam_dunks: 12,
+        },
+      ],
+    },
   }
 end
 
 # Write code here
+
+def num_points_scored(name)
+  get_player_hash(name)[:points]
+end
+
+def shoe_size(name)
+  get_player_hash(name)[:shoe]
+end
+
+def team_colors(team_name)
+  get_team_hash(team_name)[:colors]
+end
+
+def team_names
+  team_names = []
+  game_hash.each do |key, value|
+    team_names.push(value[:team_name])
+  end
+  team_names
+end
+
+def player_numbers(team_name)
+  get_team_hash(team_name)[:players].map do |player|
+    player[:number]
+  end
+end
+
+def player_stats(name)
+  get_player_hash(name)
+end
+
+def big_shoe_rebounds
+  largestShoe = 0
+  name = ""
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if player[:shoe] > largestShoe
+        name = player[:player_name]
+        largestShoe = player[:shoe]
+      end
+    end
+  end
+  get_player_hash(name)[:rebounds]
+end
+
+def most_points_scored(name)
+end
+
+#Helper methods
+def get_player_hash(name)
+  player_hash = {}
+
+  game_hash.each do |key, value|
+    if player_hash == {}
+      value[:players].find do |player|
+        if player[:player_name] == name
+          player_hash = player
+        end
+      end
+    end
+  end
+  player_hash
+end
+
+def get_team_hash(team_name)
+  team_hash = {}
+  game_hash.each do |key, value|
+    if value[:team_name] == team_name
+      team_hash = value
+    end
+  end
+  team_hash
+end
